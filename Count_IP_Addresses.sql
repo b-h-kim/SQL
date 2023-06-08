f9e3a7da59b6204f055e5ee2af251a73d2a32ba9
Byungho "B.H." Kim <59985293+b-h-kim@users.noreply.github.com>
2023-06-08 11:27:09 -0500

    Update Count_IP_Addresses.sql

--- Count_IP_Addresses.sql
+++ Count_IP_Addresses.sql
@@ -1,32 +1,32 @@
 /*		
-	DESCRIPTION:
-	Given a database of first and last IPv4 addresses, calculate the number of addresses between them (including the first one, excluding the last one).
-	
-	Input
-	---------------------------------
-	|     Table    | Column | Type  |
-	|--------------+--------+-------|
-	| ip_addresses | id     | int   |
-	|              | first  | text  |
-	|              | last   | text  |
-	---------------------------------
-	Output
-	-------------------------
-	|   Column    |  Type   |
-	|-------------+---------|
-	| id          | int     |
-	| ips_between | bigint  |
-	-------------------------
-	All inputs will be valid IPv4 addresses in the form of strings. The last address will always be greater than the first one.
-	
-	Examples
-	* With input "10.0.0.0", "10.0.0.50"  => return   50 
-	* With input "10.0.0.0", "10.0.1.0"   => return  256 
-	* With input "20.0.0.10", "20.0.1.0"  => return  246
+**	DESCRIPTION:
+**	Given a database of first and last IPv4 addresses, calculate the number of addresses between them (including the first one, excluding the last one).
+**	
+**	Input
+**	---------------------------------
+**	|     Table    | Column | Type  |
+**	|--------------+--------+-------|
+**	| ip_addresses | id     | int   |
+**	|              | first  | text  |
+**	|              | last   | text  |
+**	---------------------------------
+**	Output
+**	-------------------------
+**	|   Column    |  Type   |
+**	|-------------+---------|
+**	| id          | int     |
+**	| ips_between | bigint  |
+**	-------------------------
+**	All inputs will be valid IPv4 addresses in the form of strings. The last address will always be greater than the first one.
+**	
+**	Examples
+**	* With input "10.0.0.0", "10.0.0.50"  => return   50 
+**	* With input "10.0.0.0", "10.0.1.0"   => return  256 
+**	* With input "20.0.0.10", "20.0.1.0"  => return  246
 */
 
 /*
-	ANSWEAR
+**	ANSWEAR
 */
 
 WITH ip_splited AS
@@ -40,5 +40,5 @@ WITH ip_splited AS
   )
 
 SELECT  id
-        ,(diff_1 + diff_2 + diff_3 + diff_4)::BIGINT AS ips_between 
+        ,(diff_1 + diff_2 + diff_3 + diff_4)::INT AS ips_between 
 FROM    ip_splited
